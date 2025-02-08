19297e2e19a5a7f76d05250aa73dceab6594ce9f
arki <antoniorafaelariasromero@gmail.com>
2025-02-07 23:39:13 -0500

    feat: Add CORS support and update npm scripts
    - Add Cross-Origin Resource Sharing (CORS) middleware to enable frontend
    integration
    
    Also reorganize npm scripts for better development workflow:
    - Add CORS package and middleware configuration
    - Split start script into production and development versions
    - Rename development script to use --env-file flag
    - Update watch script to use dev command

--- api/main.js
+++ api/main.js
@@ -2,12 +2,14 @@ import express from 'express';
 import * as config from './config/index.js';
 import connectDB from "./config/db.js";
 import router from "./routes/index.js";
+import cors from 'cors'
 
 const app = express();
 const PORT = config.PORT || 3000;
 
 connectDB();
 
+app.use(cors())
 app.use(express.json());
 app.use('/api/tasks', router);
 
