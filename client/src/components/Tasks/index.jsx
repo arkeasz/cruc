3bdd17429c1e8bc1fee795919578927475726282
arki <antoniorafaelariasromero@gmail.com>
2025-02-09 11:23:19 -0500

    refactor(client): optimize task state management
    - Refactored the task state management by moving logic to the `useTasks` hook
    - Improved modal styling and button alignment in `index.css`

--- client/src/components/Tasks/index.jsx
+++ client/src/components/Tasks/index.jsx
@@ -2,99 +2,22 @@ import { useEffect, useState } from 'react'
 import Task from '../Task';
 import './index.css';
 import FormTask from '../FormTask';
-const API_URL = import.meta.env.VITE_API_URL;
+import { useTasks } from '../../hooks/useTasks';
+
 
 function Tasks()  {
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(API_URL);
-          if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-          }
-          const result = await response.json();
-          setData(result);
-        } catch (err) {
-          setError(err.message)
-        } finally {
-          setLoading(false)
-        }
-      };
-
-      fetchData()
-    }, []);
-
-    const addTask = async () => {
-      if (!title) return;
-      try {
-        const response = await fetch(`${API_URL}`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ title, description })
-        });
-
-        if (!response.ok)  {
-          throw new Error("Error to post task");
-        }
-        const newTask = await response.json();
-
-        setData((prevData) => {
-          const exists = prevData.some((task) => task._id === newTask._id);
-          return exists ? prevData : [...prevData, newTask];
-        });
-        setTitle("");
-        setDescription("");
-      } catch (err) {
-        console.log("Error:", err)
-      }
-    };
-
-    const deleteTask = async (id) => {
-      try {
-        const response = await fetch(`${API_URL}/${id}`, {
-          method: "DELETE",
-        });
-
-        if (!response.ok) {
-          throw new Error("Error al eliminar la tarea");
-        }
-
-        setData((prevData) => prevData.filter((task) => task._id !== id));
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-
-    const updateTask = async (id, updateData) =>  {
-      try {
-        const response = await fetch(`${API_URL}/${id}`,  {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(updateData)
-        });
-
-        if (!response.ok) {
-          throw new Error("Error al actualizar la tarea");
-        }
-
-        const updTask = await response.json();
-        setData(prevData =>
-          prevData.map(task =>
-            task._id === id ? { ...task, ...updateData } : task
-          )
-        );
-      } catch (err) {
-        console.error("Error", err.message)
-      }
-    };
+    const {
+      data,
+      error,
+      loading,
+      title,
+      description,
+      addTask,
+      deleteTask,
+      updateTask,
+      setDescription,
+      setTitle
+    } = useTasks();
 
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error}</p>;
--- client/src/hooks/useTasks.js
+++ client/src/hooks/useTasks.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { fetchTasks, addTask as addTaskApi, deleteTask as deleteTaskApi } from '../services/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const useTasks = () => {
   const [data, setData] = useState([]);
@@ -9,39 +9,100 @@ export const useTasks = () => {
   const [description, setDescription] = useState("");
 
   useEffect(() => {
-    const loadTasks = async () => {
+    const fetchData = async () => {
       try {
-        const tasks = await fetchTasks();
-        setData(tasks);
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+        setData(result);
       } catch (err) {
-        setError(err.message);
+        setError(err.message)
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
     };
 
-    loadTasks();
+    fetchData()
   }, []);
 
+
   const addTask = async () => {
+    if (!title) return;
     try {
-      const newTask = await addTaskApi(title, description);
-      setData((prevData) => [...prevData, newTask]);
+      const response = await fetch(`${API_URL}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title, description })
+      });
+
+      if (!response.ok)  {
+        throw new Error("Error to post task");
+      }
+      const newTask = await response.json();
+
+      setData((prevData) => {
+        const exists = prevData.some((task) => task._id === newTask._id);
+        return exists ? prevData : [...prevData, newTask];
+      });
       setTitle("");
       setDescription("");
     } catch (err) {
-      console.error(err);
+      console.log("Error:", err)
     }
   };
 
   const deleteTask = async (id) => {
     try {
-      await deleteTaskApi(id);
+      const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al eliminar la tarea");
+      }
+
       setData((prevData) => prevData.filter((task) => task._id !== id));
-    } catch (err) {
-      console.error(err);
+    } catch (error) {
+      console.error("Error:", error);
     }
   };
 
-  return { data, loading, error, title, description, setTitle, setDescription, addTask, deleteTask };
+  const updateTask = async (id, updateData) =>  {
+    try {
+      const response = await fetch(`${API_URL}/${id}`,  {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updateData)
+      });
+
+      if (!response.ok) {
+        throw new Error("Error al actualizar la tarea");
+      }
+
+      setData(prevData =>
+        prevData.map(task =>
+          task._id === id ? { ...task, ...updateData } : task
+        )
+      );
+    } catch (err) {
+      console.error("Error", err.message)
+    }
+  };
+
+  return {
+    data,
+    loading,
+    error,
+    title,
+    description,
+    setTitle,
+    setDescription,
+    addTask,
+    deleteTask,
+    updateTask
+  };
 };
